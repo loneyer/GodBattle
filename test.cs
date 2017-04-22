d7d75d121ad317b7efe443a5ae9ca3d4adf3e7af
loneyer <striver@linguangzhideMacBook-Pro.local>
2017-04-22 10:31:47 +0800

    t

---
+++ test.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		DCAgent.getInstance().initWithAppIdAndChannelId("468A028FD4E34D8C6B29584F67302E03", "1001");
+
+		DCEvent.onEvent ("SignInTab_Btn");
+
+		StartCoroutine (showLog ());
+	}
+
+	IEnumerator showLog(){
+		yield return null;
+
+		DCEvent.onEvent ("showLog");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
